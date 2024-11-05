6e018a6dd20b5090f527b7758425483ae8297a17
Caleb Akai <caleb.akai@outlook.com>
2024-11-05 15:26:16 -0500

    feat: added .java files, rough welcome page

---
+++ app/src/main/java/com/b07/planetze/WelcomeFragment.java
@@ -0,0 +1,45 @@
+package com.b07.planetze;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+public class WelcomeFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        Button buttonRegister = view.findViewById(R.id.buttonRegister);
+        Button buttonLogin = view.findViewById(R.id.buttonLogin);
+
+        /*
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new RegisterFragment());        <- create register fragment/page
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new LoginFragment());          <- create login fragment/page
+            }
+        });*/
+
+        return view;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
